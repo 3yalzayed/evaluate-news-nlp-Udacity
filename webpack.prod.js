c72ee880cc818a36047b6cc38ea75cb78cacd04c
Sultan Khaled <72667717+3yalzayed@users.noreply.github.com>
2021-01-12 22:11:37 +0400

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,21 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-// const WorkboxPlugin = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "production",
+
   output: {
     libraryTarget: "var",
     library: "Client",
+    // optimization: {
+    //   minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    // },
   },
   module: {
     rules: [
@@ -20,6 +27,7 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
+        // use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
@@ -28,6 +36,8 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
-    //new WorkboxPlugin.GenerateSW(),
+    new MiniCssExtractPlugin({ filename: "[name].css" }),
+
+    // new WorkboxPlugin.GenerateSW(),
   ],
 };
