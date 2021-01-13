a026fdca71118b4e29bdc46a9ff1f20bcb46ed38
Sultan Khaled <72667717+3yalzayed@users.noreply.github.com>
2021-01-13 21:57:12 +0400

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,6 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -37,7 +36,5 @@ module.exports = {
       filename: "./index.html",
     }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
-
-    // new WorkboxPlugin.GenerateSW(),
   ],
 };
