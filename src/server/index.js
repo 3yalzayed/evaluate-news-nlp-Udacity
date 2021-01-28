9e56ccf8de8d2d314619efcc5ce1143d26c708b8
Sultan Khaled <72667717+3yalzayed@users.noreply.github.com>
2021-01-28 13:04:40 +0400

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,33 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const path = require("path");
+const mockAPIResponse = require("./mockAPI.js");
+const fetch = require("node-fetch");
+
 dotenv.config();
 
-projectData = {};
-
-// set aylien API credentias
-var textapi = {
-  application_key: `${process.env.API_KEY}`,
-};
-
-console.log(`Your API key is ${process.env.API_KEY}`);
-
 // variables
-var express = require("express");
-const bodyParser = require("body-parser");
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const fetch = require("node-fetch");
 // Initialize the main project folder
 app.use(express.static("dist"));
-
-const mockAPIResponse = require("./mockAPI.js");
-var path = require("path");
 
 // configuring express
 app.use(express.json());
-
-// Initialize the main project folder
-app.use(express.static("dist"));
 
 // Cors for cross origin allowance
 var cors = require("cors");
@@ -36,29 +25,31 @@ app.use(cors());
 console.log(__dirname);
 
 app.get("/", function (req, res) {
-  res.sendFile("dist/index.html");
+  // res.sendFile("dist/index.html");
   res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-  console.log("Example app listening on port 8081!");
+
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
 });
 
-// app.get("/test", function (req, res) {
-//   res.send(mockAPIResponse);
-// });
-
-app.post("/addURL", addURL);
+app.post("/add-url", addURL);
 async function addURL(req, res) {
-  const ras = await fetch(
-          `https://api.meaningcloud.com/sentiment-2.1?key=${process.env.API_KEY}&lang=auto&url=${req.body.url}`  );
+  const baseURL = "https://api.meaningcloud.com/sentiment-2.1?";
+  const apiURL =
+    baseURL + `key=${process.env.API_KEY}&lang=auto&url=${req.body.url}`;
+  const raspone = await fetch(apiURL);
   try {
-    const data = await ras.json();
+    const data = await raspone.json();
     console.log(data);
-    return data;
     res.send(data);
   } catch (error) {
     console.log("error", error);
   }
 }
+
+app.listen(8081, function () {
+  console.log("Example app listening on port 8081!");
+});
